9d70b495f2e898442ee666a9f82becf54c9aa27a
Jędrzej Ratajczak <nilphym@gmail.com>
2024-07-02 18:54:20 +0200

    Update List.js

--- week-2/List.js
+++ week-2/List.js
@@ -24,6 +24,7 @@ class List {
 // 3. Write the time and space complexity of each method. (optional)
 
 // Notes:
+// - Restrict the solution to work on the primitive types.
 // - Pay attention to edge cases, such as clearing an empty list, removing a non-existent element, modifying an element at a non-existent index, etc.
 // - Do not use any array functions; instead, create a new array with a different size, and search/count elements using loops.
 // - Additional task: The constructor creates an array of a certain size, e.g., 5, and when this size is exceeded, the size is doubled. You may need variables to store the current and maximum array size.
