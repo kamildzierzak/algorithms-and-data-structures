5a16e64f13b7a52ac814ff7244750289f7e1b70c
Jędrzej Ratajczak <nilphym@gmail.com>
2024-07-07 11:41:26 +0200

    Update List.js

--- week-2/List.js
+++ week-2/List.js
@@ -14,12 +14,12 @@ class List {
 
 // Tasks:
 // Implement the List class according to the above interface.
-// Implement tests. (optional)
-// Write the time and space complexity of each method. (optional)
+// Optional: Implement tests.
+// Optional: Write the time and space complexity of each method.
 
 // Notes:
 // You can use the Array constructor to define the array of a specific size.
 // Restrict the solution to work on the primitive types.
 // Pay attention to edge cases, such as clearing an empty list, removing a non-existent element, modifying an element at a non-existent index, etc.
 // Do not use any array functions; instead, create a new array with a different size, and search/count elements using loops.
-// Additional task: The constructor creates an array of a certain size, e.g., 5, and when this size is exceeded, the size is doubled. You may need variables to store the current and maximum array size.
+// Optional: The constructor creates an array of a certain size, eg. 5, and when this size is exceeded, the size is doubled. You may need variables to store the current and maximum array size.
