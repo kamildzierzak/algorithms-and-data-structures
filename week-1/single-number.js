9dc0dcd9a803ee37b29dcc66a3a85db94a225046
Kamil Dzierzak <kamdzierzak@gmail.com>
2024-08-17 23:25:45 +0200

    refactor(week-1): solve single-number by using XOR

--- week-1/single-number.js
+++ week-1/single-number.js
@@ -1,14 +1,26 @@
-let singleNumber = function (nums) {
-  // O(n log n)
-  nums.sort((x, y) => x - y);
+// XOR way
+let singleNumber = nums => {
+  let theChoosenOne = nums[0];
 
   // O(n)
-  for (let i = 0; i < nums.length; i += 2) {
-    // O(1)
-    if (nums[i] !== nums[i + 1]) {
-      return nums[i];
-    }
+  for (let i = 1; i < nums.length; i++) {
+    theChoosenOne = theChoosenOne ^ nums[i];
   }
+
+  return theChoosenOne;
 };
 
-// Worse time complexity: O(n log n)
+// let singleNumber = function (nums) {
+//   // O(n log n)
+//   nums.sort((x, y) => x - y);
+
+//   // O(n)
+//   for (let i = 0; i < nums.length; i += 2) {
+//     // O(1)
+//     if (nums[i] !== nums[i + 1]) {
+//       return nums[i];
+//     }
+//   }
+// };
+
+// // Worse time complexity: O(n log n)
